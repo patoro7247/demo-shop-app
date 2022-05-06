3afca418a5af47fe90d3f2b6977c7f6e60a7e872
Patrick Harding <patrickhardingr@gmail.com>
2022-05-06 16:49:40 -0700

    more changes

---
+++ frontend/src/components/MainWrapper.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+import {CardWrapper} from './styles/CardWrapper.styled.js';
+import CardRow from './styles/CardRow.js'
+
+
+
+const MainWrapper = styled.div`
+    width: 80vw;
+    padding-top: 11vh;
+    padding-bottom: 5vh;
+    margin: 0 auto;
+    background-color: rgb(224, 222, 215);
+    height: 95vh;
+`
+
+export default MainWrapper;
--- frontend/src/components/main.js
+++ frontend/src/components/main.js
@@ -5,22 +5,8 @@ import {CardWrapper} from './styles/CardWrapper.styled.js';
 import CardRow from './styles/CardRow.js'
 
 
+ const Main = (props) => {
 
-export const MainWrapper = styled.div`
-    width: 80vw;
-    padding-top: 11vh;
-    padding-bottom: 5vh;
-    margin: 0 auto;
-    background-color: rgb(224, 222, 215);
-    height: 95vh;
-`
-
-
-
-
-export const Main = (props) => {
-    
-    const {data} = []
 
     return (
 
